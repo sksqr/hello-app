97d74feb7957a1a5fa98fab58ce633592bf4d7b3
shashikant-sf <shashikant.kushwaha@salesforce.com>
2025-03-14 22:16:00 +0530

    Basic Web App

---
+++ src/test/java/com/example/hello_app/HelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
