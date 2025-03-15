36a98b78b53f7fab005e52040e3235678b718e0e
shashikant-sf <shashikant.kushwaha@salesforce.com>
2025-03-15 12:02:14 +0530

    Removed greeting API

--- src/main/java/com/example/hello_app/HelloController.java
+++ src/main/java/com/example/hello_app/HelloController.java
@@ -10,26 +10,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello JBDL-79 - "+Thread.currentThread().getName();
+        return "Hello JBDL - "+Thread.currentThread().getName();
     }
 
-
-    @GetMapping("/greeting")
-    public String greeting() {
-        LocalTime now = LocalTime.now();
-        int hour = now.getHour();
-
-        String greeting;
-        if (hour >= 5 && hour < 12) {
-            greeting = "Good Morning";
-        } else if (hour >= 12 && hour < 17) {
-            greeting = "Good Afternoon";
-        } else if (hour >= 17 && hour < 21) {
-            greeting = "Good Evening";
-        } else {
-            greeting = "Good Night";
-        }
-
-        return greeting + "! Current server time: " + now;
-    }
 }
