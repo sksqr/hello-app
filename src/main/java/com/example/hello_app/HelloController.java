ad63f9fd405018e0418d2b5ff5a5ba9feff558de
shashikant-sf <shashikant.kushwaha@salesforce.com>
2025-03-15 11:27:55 +0530

    Added greeting API

--- src/main/java/com/example/hello_app/HelloController.java
+++ src/main/java/com/example/hello_app/HelloController.java
@@ -3,11 +3,33 @@ package com.example.hello_app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String hello(){
         return "Hello JBDL-79 - "+Thread.currentThread().getName();
+    }
+
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+
+        String greeting;
+        if (hour >= 5 && hour < 12) {
+            greeting = "Good Morning";
+        } else if (hour >= 12 && hour < 17) {
+            greeting = "Good Afternoon";
+        } else if (hour >= 17 && hour < 21) {
+            greeting = "Good Evening";
+        } else {
+            greeting = "Good Night";
+        }
+
+        return greeting + "! Current server time: " + now;
     }
 }
