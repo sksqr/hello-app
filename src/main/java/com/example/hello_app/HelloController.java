fb43258c6cb583031b95d2db840cd6128942a7a7
shashikant-sf <shashikant.kushwaha@salesforce.com>
2025-03-15 10:47:26 +0530

    Changed Controller

--- src/main/java/com/example/hello_app/HelloController.java
+++ src/main/java/com/example/hello_app/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello JBDL - "+Thread.currentThread().getName();
+        return "Hello JBDL-79 - "+Thread.currentThread().getName();
     }
 }
